f1154d6cc227417ea857c52cffec07133f20a2dc
Tamás Losonczi <losonczi.tamas91@gmail.com>
2021-04-26 08:40:07 +0200

    draw gizmos for tiles to indicate type

--- Assets/Scripts/Prototype01/MouseToTile.cs
+++ Assets/Scripts/Prototype01/MouseToTile.cs
@@ -62,7 +62,28 @@ namespace Prototype01
                 }
             }
         }
-        
+
+        private void OnDrawGizmos()
+        {
+            for (var i = 0; i < _tileMapPropses.Length; i++)
+            {
+                var tileMap = _tileMapPropses[i].Tilemap;
+                var offset = _tileMapPropses[i].Offset;
+                foreach (var boundInt in tileMap.cellBounds.allPositionsWithin)
+                {
+                    var relativePos = new Vector3Int(boundInt.x, boundInt.y, boundInt.z);
+                    if (tileMap.HasTile(relativePos))
+                    {
+                        var tile = tileMap.GetTile(relativePos);
+                        var walkable = tile.GetType() == typeof(WalkableTile);
+                        var worldPos = tileMap.CellToWorld(relativePos);
+                        Gizmos.color = walkable ? Color.green : Color.red;
+                        Gizmos.DrawCube(worldPos + offset, Vector3.one * 0.1f);
+                    }
+                }
+            }
+        }
+
         public bool TileIsWalkable(Vector3 worldPos)
         {
             for (var i = 0; i < _tileMapPropses.Length; i++)
--- Assets/Scripts/Prototype01/WalkableTile.cs
+++ Assets/Scripts/Prototype01/WalkableTile.cs
@@ -1,9 +1,23 @@
+using UnityEditor;
 using UnityEngine.Tilemaps;
 
 namespace Prototype01
 {
-    public class WalkableTile : TileBase
+    public class WalkableTile : Tile
     {
-        
+#if UNITY_EDITOR
+        [MenuItem("Assets/Create/Walkable Tile")]
+        public static void CreatePrefabTiles()
+        {
+            var path = EditorUtility.SaveFilePanelInProject("Save Prefab Tile", "New Prefab Tile", "asset", "Save Prefab Tile", "Assets");
+
+            if (path == "")
+            {
+                return;
+            }
+ 
+            AssetDatabase.CreateAsset(CreateInstance<WalkableTile>(), path);
+        }
+#endif
     }
 }
