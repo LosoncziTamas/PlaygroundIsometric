fcbdd81921b61311e4aa875a2eb323bb61360565
Tamás Losonczi <losonczi.tamas91@gmail.com>
2021-05-31 19:22:16 +0200

    extract path finder to separate class

--- Assets/Scripts/Prototype01/CharacterMovement.cs
+++ Assets/Scripts/Prototype01/CharacterMovement.cs
@@ -15,6 +15,7 @@ namespace Prototype01
 
         [SerializeField] private float _speed = 2.0f;
         [SerializeField] private TileMapper _tileMapper;
+        [SerializeField] private PathFinder _pathFinder;
 
         private MouseInput _mouseInput;
         private Vector3? _destination;
@@ -42,7 +43,7 @@ namespace Prototype01
             if (tilePos.HasValue)
             {
                 _destination = tilePos.Value;
-                var path = FindPath(transform.position, tilePos.Value);
+                var path = _pathFinder.FindPath(transform.position, tilePos.Value);
                 StopAllCoroutines();
                 StartCoroutine(MoveAlongPath(path));
                 _path.Clear();
@@ -84,86 +85,11 @@ namespace Prototype01
 
             foreach (var node in _path)
             {
-                var worldPos = _tileMapper.CellToWorldPos(node.Cell).Value;
+                var worldPos = _tileMapper.CellToWorldPos(node.Cell).GetValueOrDefault();
                 Handles.Label(worldPos, node.ToString());
                 Gizmos.color = Color.magenta;
                 Gizmos.DrawCube(worldPos,Vector3.one * 0.15f);
             }
-        }
-
-        private IList<Node> FindPath(Vector3 start, Vector3 end)
-        {
-            var openNodes = new Heap<Node>(_tileMapper.TotalCellCount);
-            var closedNodes = new HashSet<Node>();
-
-            var startCell = _tileMapper.WorldPosToCell(start);
-            var endCell = _tileMapper.WorldPosToCell(end);
-
-            if (!startCell.HasValue || !endCell.HasValue)
-            {
-                return Array.Empty<Node>();
-            }
-            
-            var startNode = new Node(startCell.Value, start, startCell.Value, endCell.Value);
-            openNodes.AddItem(startNode);
-
-            while (openNodes.Count > 0)
-            {
-                var currNode = openNodes.RemoveFirst();
-                closedNodes.Add(currNode);
-                
-                if (currNode.OnSameCell(endCell.Value))
-                {
-                    return RetracePath(startNode, currNode);
-                }
-                
-                var neighbourCells = _tileMapper.GetNeighbourCells(currNode.Cell);
-                foreach (var neighbourCell in neighbourCells)
-                {
-                    var neighbourNode = new Node(neighbourCell, _tileMapper.CellToWorldPos(neighbourCell).GetValueOrDefault(), startCell.Value, endCell.Value);
-                    if (closedNodes.Contains(neighbourNode))
-                    {
-                        continue;
-                    }
-
-                    var movementCostToNeighbour = currNode.GCost + Node.CellDistance(currNode.Cell, neighbourNode.Cell);
-                    var foundMoreOptimalPath = movementCostToNeighbour < neighbourNode.GCost;
-                    if (foundMoreOptimalPath)
-                    {
-                        Debug.Log("Found a more optimal way.");
-                    }
-                    if (foundMoreOptimalPath || !openNodes.Contains(neighbourNode))
-                    {
-                        neighbourNode.GCost = movementCostToNeighbour;
-                        neighbourNode.Parent = currNode;
-
-                        if (!openNodes.Contains(neighbourNode))
-                        {
-                            openNodes.AddItem(neighbourNode);
-                        }
-                        else
-                        {
-                            openNodes.UpdateItem(neighbourNode);
-                        }
-                    }
-                }
-            }
-
-            return Array.Empty<Node>();
-        }
-
-        private static List<Node> RetracePath(Node start, Node end)
-        {
-            var path = new List<Node>();
-            var curr = end;
-            path.Add(curr);
-            while (curr != null && !Equals(curr.Parent, start))
-            {
-                curr = curr.Parent;
-                path.Add(curr);
-            }
-            path.Reverse();
-            return path;
         }
 
         private void OnDisable()
---
+++ Assets/Scripts/Prototype01/PathFinder.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Prototype01
+{
+    public class PathFinder : MonoBehaviour
+    {
+        [SerializeField] private TileMapper _tileMapper;
+
+        private void Awake()
+        {
+            StartCoroutine(FindPath(new FindPathRequest()));
+        }
+
+        public struct FindPathRequest
+        {
+            public Vector3 Start;
+            public Vector3 End;
+            public Action<bool, Vector3[]> Callback;
+        }
+
+        public IEnumerator FindPath(FindPathRequest request)
+        {
+            Debug.Log("[PathFinder] FindPath");
+            yield return null;
+        }
+        
+        public IList<Node> FindPath(Vector3 start, Vector3 end)
+        {
+            var openNodes = new Heap<Node>(_tileMapper.TotalCellCount);
+            var closedNodes = new HashSet<Node>();
+
+            var startCell = _tileMapper.WorldPosToCell(start);
+            var endCell = _tileMapper.WorldPosToCell(end);
+
+            if (!startCell.HasValue || !endCell.HasValue)
+            {
+                return Array.Empty<Node>();
+            }
+            
+            var startNode = new Node(startCell.Value, start, startCell.Value, endCell.Value);
+            openNodes.AddItem(startNode);
+
+            while (openNodes.Count > 0)
+            {
+                var currNode = openNodes.RemoveFirst();
+                closedNodes.Add(currNode);
+                
+                if (currNode.OnSameCell(endCell.Value))
+                {
+                    return RetracePath(startNode, currNode);
+                }
+                
+                var neighbourCells = _tileMapper.GetNeighbourCells(currNode.Cell);
+                foreach (var neighbourCell in neighbourCells)
+                {
+                    var neighbourNode = new Node(neighbourCell, _tileMapper.CellToWorldPos(neighbourCell).GetValueOrDefault(), startCell.Value, endCell.Value);
+                    if (closedNodes.Contains(neighbourNode))
+                    {
+                        continue;
+                    }
+
+                    var movementCostToNeighbour = currNode.GCost + Node.CellDistance(currNode.Cell, neighbourNode.Cell);
+                    var foundMoreOptimalPath = movementCostToNeighbour < neighbourNode.GCost;
+                    if (foundMoreOptimalPath)
+                    {
+                        Debug.Log("Found a more optimal way.");
+                    }
+                    if (foundMoreOptimalPath || !openNodes.Contains(neighbourNode))
+                    {
+                        neighbourNode.GCost = movementCostToNeighbour;
+                        neighbourNode.Parent = currNode;
+
+                        if (!openNodes.Contains(neighbourNode))
+                        {
+                            openNodes.AddItem(neighbourNode);
+                        }
+                        else
+                        {
+                            openNodes.UpdateItem(neighbourNode);
+                        }
+                    }
+                }
+            }
+
+            return Array.Empty<Node>();
+        }
+
+        private static List<Node> RetracePath(Node start, Node end)
+        {
+            var path = new List<Node>();
+            var curr = end;
+            path.Add(curr);
+            while (curr != null && !Equals(curr.Parent, start))
+            {
+                curr = curr.Parent;
+                path.Add(curr);
+            }
+            path.Reverse();
+            return path;
+        }
+    }
+}
